01e0a9df1208cd0fca323a06e5d5ce16b7cbf693
Danielasalazaranaya <slalzardaniela05@gmail.com>
2022-11-30 14:08:10 -0500

    operadores

---
+++ TALLERDEOPERADORES/tallerdeoperadore.js
@@ -0,0 +1,20 @@
+// ejercicio 1
+console.log( 40  +  2  *  7 ** 2 )
+consola.log( 3 +  ( 2 + 5 ) * 1 +  ( 3 - 2 * 3 ) )
+console.log( 16  -  8  *  ( 10  -  ( 5  +  4 ) ) )
+console.log( ( 5 + 3 * 2 ) - ( 7 * 2 ) * ( 3 +  ( 2 * 3 - 1 ) ) )
+console.log( ( 24 + 220 ) / 3 * 2 )
+console.log( ( 6 / 3 * 2 ) - ( 7 * 2 ) +  ( 2 * ( 3 +  ( 2 / 3 - 1 ) * 4 ) ) )
+
+// // 2.
+console.log( 29.7  +  5.0 ** 2.0 )
+console.log(  ( 2  -  3 ) ** 4  *  5  /  ( 4  +  3  *  9 )  )
+console.log( 49,38  +  127,73  -  15,02  *  6,83  /  3,22 )
+console.log ( Math . trunc ( 19 / 3 ) )
+consola.log( 15 % 6 )
+console.log  ( ( ( 15  *  8 % 6  +  ( Math . trunc ( 24 / 2 ) ) ** 3 ) ** 3  /  4 )  *  ( 5 * 1  /  2  +  1  /  4  +  2 * 3  -  4 ) )
+// // 3.
+console.log( true  ||  true  &&  false  ||  true )
+console.log( !  (  ( false  ||  false )  &&  ( true  ||  true )  ||  true  ) )
+console.log ( !  false  &&  ( false  ||  true )  &&  ( !  true  &&  false ) )
+console.log( !  (  true  ||  false )  &&  ( false  &&  true ) )
